ce3fc8fca8634e261a8932ce63d044127bc8b8ff
Hasan Qasim <hasanqasim92@gmail.com>
2019-10-19 16:28:42 +1100

    Initial Commit

---
+++ iBeacons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBeacons
+//
+//  Created by Hasan Qasim on 19/10/19.
+//  Copyright © 2019 Hasan Qasim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
